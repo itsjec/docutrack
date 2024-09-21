d67277346af3a0225f4638432a8956892531db3a
jec <jramos112802@gmail.com>
2024-09-21 23:23:58 +0800

    removed index.html

--- app/Views/Office/include/partialnavbar.php
+++ app/Views/Office/include/partialnavbar.php
@@ -1,8 +1,8 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="navbar-brand-wrapper d-flex justify-content-center" style="background: linear-gradient(135deg, #9220b9, #C36EB8);">
         <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
-          <a class="navbar-brand brand-logo" href="index.html"><img src="assets/images/logo.png" alt="logo"/></a>
-          <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/logo-mini.svg" alt="logo"/></a>
+          <a class="navbar-brand brand-logo" href="<?= site_url('index') ?>"><img src="assets/images/logo.png" alt="logo"/></a>
+          <a class="navbar-brand brand-logo-mini" href="<?= site_url('index') ?>"><img src="images/logo-mini.svg" alt="logo"/></a>
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
             <span class="typcn typcn-th-menu"></span>
           </button>
