31d4c58248157b8918c2407a88c9d25b9ec46b86
patrick333c <jdmalaluan2@gmail.com>
2024-10-23 17:07:20 +0800

    logo pwa

--- app/Views/LogIn.php
+++ app/Views/LogIn.php
@@ -108,7 +108,18 @@
 
   <!-- endinject -->
 
-
+  <script>
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+            navigator.serviceWorker.register('/service-worker.js')
+                .then(function(registration) {
+                    console.log('Service Worker registered with scope: ', registration.scope);
+                }, function(err) {
+                    console.log('Service Worker registration failed: ', err);
+                });
+        });
+    }
+</script>
 </body>
 
 </html>
